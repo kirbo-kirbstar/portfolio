de310fd4943f809b789ad6017681e5dcf6aee880
Marlene <152890803+kirbo-kirbstar@users.noreply.github.com>
2024-04-09 14:46:53 -0400

    Add files via upload
    New bio
    Comics is the new index page
    Comics have years
    Context added to Storyboards
    New navigation links
    No more contact page. Email is now in the About page.

--- js/navbox.js
+++ js/navbox.js
@@ -1,9 +1,7 @@
 document.querySelector(".navigation").innerHTML = `
         <nav>
-            <a href="index.html">Illustration</a> |
-            <a href="comics.html">Comics</a> | 
+            <a href="index.html">Comics</a> |
             <a href="storyboards.html">Storyboards</a> |
-            <a href="about.html">About</a> | 
-            <a href="contact.html">Contact</a>
+            <a href="about.html">About</a>
         </nav>
 `;
