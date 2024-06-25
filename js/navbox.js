6e072dfe226535639d5082fb785d10dd32e05c4e
Marlene <152890803+kirbo-kirbstar@users.noreply.github.com>
2024-06-25 12:08:31 -0400

    Add files via upload
    Adding the landing page finally! Also switched Comics and Storyboards around in the navbox.

--- js/navbox.js
+++ js/navbox.js
@@ -1,7 +1,8 @@
 document.querySelector(".navigation").innerHTML = `
         <nav>
-            <a href="index.html">Comics</a> |
+            <a href="index.html">Home</a> |            
             <a href="storyboards.html">Storyboards</a> |
+            <a href="comics.html">Comics</a> |
             <a href="about.html">About</a>
         </nav>
 `;
