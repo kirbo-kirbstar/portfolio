fced16ac28d60aa2a37a6e531dfbf8e48c7fd412
Marlene <152890803+kirbo-kirbstar@users.noreply.github.com>
2025-03-11 14:56:19 -0400

    Add files via upload

--- js/navbox.js
+++ js/navbox.js
@@ -2,6 +2,7 @@ document.querySelector(".navigation").innerHTML = `
         <nav>
             <b>
             <a href="index.html">Storyboards</a> |
+            <a href="films.html">Films</a> |
             <a href="about.html">About</a>
             </b>
             <br>
